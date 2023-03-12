d2381d54564d47b9095c765a72957b54feb0d5c0
Jackie Keffas <jkeffas@gmail.com>
2023-03-12 16:46:09 -0400

    Moving the templates directory

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ import os
 from flask import Flask, render_template
 
 
-app = Flask(__name__, template_folder="../templates")
+app = Flask(__name__, template_folder="templates")
 data = json.load(
     open("./app/data/data.json")
 )
