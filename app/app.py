a6c576bf520776edfb5094e9c2bf442348db53f3
Jackie Keffas <jkeffas@gmail.com>
2023-05-01 15:35:40 -0400

    Typo fix, started photos

--- app/app.py
+++ app/app.py
@@ -25,9 +25,32 @@ def get_about():
     return render_template("about.jinja")
 
 
+@app.route("/photo")
+def get_photo():
+    return render_template("photo.jinja")
+
+
+@app.route("/photo/mythology")
+def get_photo_mythology():
+    return render_template("photo-mythology.jinja")
+
+@app.route("/photo/cemeteries")
+def get_photo_cemetereis():
+    return render_template("photo-cemeteries.jinja")
+
+@app.route("/photo/fruit")
+def get_photo_fruit():
+    return render_template("photo-fruit.jinja")
+
+@app.route("/photo/roommates")
+def get_photo_roommates():
+    return render_template("photo-roommates.jinja")
+
+
 @app.errorhandler(404)
 def get_error_page(error):
     return render_template("404.jinja"), 404
+
 
 if __name__=="__main__":
     app.run(debug=False)
